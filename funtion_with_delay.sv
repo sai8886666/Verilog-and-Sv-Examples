20367bb40058330b355d3ef9e279e97911beadd5
sai8886666 <thotasaikrishn20@gmail.com>
2024-06-17 18:18:04 +0530

    function with delay

---
+++ funtion_with_delay.sv
@@ -0,0 +1,25 @@
+
+module tb;
+  reg [2:0]a;
+  reg [2:0]b;
+  reg  [4:0]out;
+  
+  function void sum;
+    fork
+    out = a+b;
+    #100;
+      $display("*****************out:%b",out);
+    join_none
+    //#100
+    $display("**----------******out:%b",out);
+  endfunction
+  
+  initial begin
+    a=10;
+    b=3; 
+  end
+  
+  initial begin
+    sum;
+  end
+endmodule
