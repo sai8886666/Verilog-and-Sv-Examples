f19060e332ad83883fdfdda51b63bc453aea8931
sai krishna Thota <163770578+sai8886666@users.noreply.github.com>
2024-07-11 14:20:05 +0530

    Create sequence_001002003004005.sv

---
+++ Constraints/sequence_001002003004005.sv
@@ -0,0 +1,24 @@
+
+class seq;
+  randc bit[4:0] arr[$];
+  
+  constraint one {arr.size() inside {[10:20]};}
+  
+  constraint two {
+    foreach(arr[i]) 
+      if(i%3==0 || i%3==1)
+        arr[i] ==0;
+    else if (i%3==2)
+      arr[i] == (i+1)/3;
+  		}
+endclass
+
+module tb;
+  seq s;
+  
+  initial begin
+    s = new();
+    s.randomize();
+    $display(" \t arr:%p",s.arr);
+  end
+endmodule
