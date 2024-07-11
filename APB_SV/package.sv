5e3610d21863e16a00ff5c8fe106e33c908e4abc
sai krishna Thota <163770578+sai8886666@users.noreply.github.com>
2024-07-11 12:43:07 +0530

    Create package.sv

---
+++ APB_SV/package.sv
@@ -0,0 +1,15 @@
+// ------------ package --------------
+
+`include "interface.sv"
+`include "design1.sv"
+`include "transaction.sv"
+`include "generator.sv"
+`include "coverage.sv"
+`include "driver.sv"
+`include "input_monitor.sv"
+`include "output_monitor.sv"
+`include "scorecard.sv"
+`include "environment.sv"
+`include "test.sv"
+`include "top.sv"
+
