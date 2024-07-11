9bab6f26f11bacebf06ded69606d1f6c8728daa8
sai krishna Thota <163770578+sai8886666@users.noreply.github.com>
2024-07-11 14:30:20 +0530

    Create pos_neg_detc.v

---
+++ Edge_detectors/pos_neg_detc.v
@@ -0,0 +1,14 @@
+module dual_edge(in,clk,out);
+  input in;
+  input clk;
+  output reg out;
+  reg prevsig;
+  
+  
+  always @(posedge clk) begin
+    prevsig <= in;
+  end
+  
+  assign out = in ^ prevsig;
+  
+endmodule
